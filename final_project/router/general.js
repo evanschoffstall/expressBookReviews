b1a909fe84d040019cff3d900782fb202f166cb2
Evan Schoffstall <evan.schoffstall@gmail.com>
2024-04-23 16:35:40 -0400

    Satisfy requirements for project tasks 10 -13 using promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,43 +20,87 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  return res.status(200).json(books);
+public_users.get('/', function (req, res) {
+  let theseBooks = new Promise((resolve, reject) => {
+    try {
+      resolve(books);
+    } catch (err) {
+      reject(err);
+    }
+  });
+
+  theseBooks.then(
+    (data) => res.status(200).json({ books: data }),
+    (err) => res.status(500).json({ error: err })
+  );
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  return res.status(200).json(books[req.params.isbn]);
- });
-  
+public_users.get('/isbn/:isbn', function (req, res) {
+  let getBookByISBN = new Promise((resolve, reject) => {
+    try {
+      resolve(books[req.params.isbn]);
+    } catch (err) {
+      reject(err);
+    }
+  });
+
+  getBookByISBN.then(
+    (data) => res.status(200).json(data),
+    (err) => res.status(500).json({ error: err })
+  );
+});
+
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
-  let booksByAuthor = [];
-  for (let book in books) {
-    if (books[book].author.toLowerCase() === req.params.author.toLowerCase()) {
-      booksByAuthor.push(books[book]);
+  let getBooksByAuthor = new Promise((resolve, reject) => {
+    try {
+      let booksByAuthor = [];
+      for (let book in books) {
+        if (books[book].author.toLowerCase() === req.params.author.toLowerCase()) {
+          booksByAuthor.push(books[book]);
+        }
+      }
+      if (booksByAuthor.length > 0) {
+        resolve(booksByAuthor);
+      } else {
+        throw new Error("No books found for this author");
+      }
+    } catch (err) {
+      reject(err);
     }
-  }
-  if (booksByAuthor.length > 0) {
-    return res.status(200).json(booksByAuthor);
-  } else {
-    return res.status(404).json({ message: "No books found for this author" });
-  }
+  });
+
+  getBooksByAuthor.then(
+    (data) => res.status(200).json(data),
+    (err) => res.status(404).json({ message: err.message })
+  );
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  let booksByTitle = [];
-  for (let book in books) {
-    if (books[book].title.toLowerCase() === req.params.title.toLowerCase()) {
-      booksByTitle.push(books[book]);
+public_users.get('/title/:title', function (req, res) {
+  let getBooksByTitle = new Promise((resolve, reject) => {
+    try {
+      let booksByTitle = [];
+      for (let book in books) {
+        if (books[book].title.toLowerCase() === req.params.title.toLowerCase()) {
+          booksByTitle.push(books[book]);
+        }
+      }
+      if (booksByTitle.length > 0) {
+        resolve(booksByTitle);
+      } else {
+        throw new Error("No books found with this title");
+      }
+    } catch (err) {
+      reject(err);
     }
-  }
-  if (booksByTitle.length > 0) {
-    return res.status(200).json(booksByTitle);
-  } else {
-    return res.status(404).json({ message: "No books found with this title" });
-  }
+  });
+
+  getBooksByTitle.then(
+    (data) => res.status(200).json(data),
+    (err) => res.status(404).json({ message: err.message })
+  );
 });
 
 //  Get book review
